01315d8e180901085447f3120c9ef61f5a5744d6
yangwei <yangwei401@xdf.cn>
2020-01-06 13:53:07 +0800

    add config

--- ultron-boss/src/main/groovy/com/ultron/boss/BossApplication.groovy
+++ ultron-boss/src/main/groovy/com/ultron/boss/BossApplication.groovy
@@ -1,14 +1,15 @@
 package com.ultron.boss
 
+import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-
 /**
  * Create By yangwei
  * Create at 2020/01/03 20:43
  * Description:
  */
 
+@MapperScan("com.ultron.boss.mapper")
 @SpringBootApplication
 class BossApplication {
 
